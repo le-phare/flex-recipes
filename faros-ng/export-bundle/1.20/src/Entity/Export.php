63d802aaf09a7c66fae5e475209cbbd370c7d4da
Thibaut Selingue <thibaut.selingue@gmail.com>
2023-02-28 17:29:02 +0100

    Update faros-ng/export-bundle/1.20/src/Entity/Export.php
    Co-authored-by: Nicolas LAURENT <aegypius@users.noreply.github.com>

--- faros-ng/export-bundle/1.20/src/Entity/Export.php
+++ faros-ng/export-bundle/1.20/src/Entity/Export.php
@@ -7,7 +7,7 @@ use Faros\Component\Export\Model\Export as ExportModel;
 use Faros\Bundle\ExportBundle\ORM\ExportRepository;
 
 /**
- * @ORM\Entity(repositoryClass="Faros\Bundle\ExportBundle\ORM\ExportRepository")
+ * @ORM\Entity(repositoryClass=ExportRepository::class)
  * @ORM\Table(name="faros_export")
  */
 #[ORM\Entity(repositoryClass: ExportRepository::class)]
