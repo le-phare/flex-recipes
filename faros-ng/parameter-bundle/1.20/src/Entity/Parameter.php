65507ab8a23e313846bf7248513301f98bba642f
Thibaut Selingue <thibaut.selingue@gmail.com>
2023-02-28 17:28:29 +0100

    Update faros-ng/parameter-bundle/1.20/src/Entity/Parameter.php
    Co-authored-by: Nicolas LAURENT <aegypius@users.noreply.github.com>

--- faros-ng/parameter-bundle/1.20/src/Entity/Parameter.php
+++ faros-ng/parameter-bundle/1.20/src/Entity/Parameter.php
@@ -7,7 +7,7 @@ use Faros\Component\Parameter\Model\Parameter as ParameterModel;
 use Faros\Bundle\ParameterBundle\ORM\ParameterRepository;
 
 /**
- * @ORM\Entity(repositoryClass="Faros\Bundle\ParameterBundle\ORM\ParameterRepository")
+ * @ORM\Entity(repositoryClass=ParameterRepository::class)
  * @ORM\Table(name="faros_parameter")
  */
 #[ORM\Entity(repositoryClass: ParameterRepository::class)]
