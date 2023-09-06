27270c026f841799131014dff0477784eb15d782
Thibaut Selingue <thibaut.selingue@lephare.com>
2023-09-06 09:26:07 +0200

    fix(export): add missing relation mapping between export and group (#29)

--- faros-ng/export-bundle/3.0/src/Entity/Export.php
+++ faros-ng/export-bundle/3.0/src/Entity/Export.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Faros\Component\Export\Model\Export as ExportModel;
 use Faros\Bundle\ExportBundle\ORM\ExportRepository;
@@ -17,4 +18,10 @@ class Export extends ExportModel
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
     protected $id;
+
+    /**
+     * @var Collection<int, Group>
+     */
+    #[ORM\ManyToMany(targetEntity: Group::class)]
+    protected $groups;
 }
