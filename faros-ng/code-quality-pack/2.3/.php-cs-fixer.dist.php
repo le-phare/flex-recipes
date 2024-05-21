9ee8241d2564fe8c23ff841e9f95ece7cb6c2650
Pierre Boissinot <pierre.boissinot@lephare.com>
2024-05-21 16:19:22 +0200

    feat(code-quality-pack): Code quality pack editorconfig line max length (#35)
    * chore: init code-quality-pack 2.3 version from 2.2
    
    * feat(code-quality-pack): set max_line_length in .editorconfig
    
    * feat(code-quality-pack): respect PSR
    
    * feat(code-quality-pack): add return
    
    * feat(code-quality-pack): add return

---
+++ faros-ng/code-quality-pack/2.3/.php-cs-fixer.dist.php
@@ -0,0 +1,29 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->in(__DIR__.'/src')
+    ->in(__DIR__.'/migrations')
+    ->in(__DIR__.'/tests')
+;
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@Symfony' => true,
+        '@DoctrineAnnotation' => true,
+        'no_unused_imports' => true,
+        'array_syntax' => [
+            'syntax' => 'short',
+        ],
+        'ordered_imports' => [
+            'sort_algorithm' => 'alpha',
+        ],
+        'multiline_whitespace_before_semicolons' => [
+            'strategy' => 'new_line_for_chained_calls',
+        ],
+        'phpdoc_separation' => [
+            'groups' => [['ORM\\*'], ['Assert\\*']],
+        ]
+    ])
+    ->setFinder($finder)
+    ->setCacheFile('.php-cs-fixer.cache') // forward compatibility with 3.x line
+;
