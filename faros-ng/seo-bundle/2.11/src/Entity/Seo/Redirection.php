3e675d83966449ebfdb71ceab7252931f1d19afb
SimonRethore <srethore.pro@gmail.com>
2024-05-30 11:57:08 +0200

    feat(seo): add redirection repo (#39)
    * Add Redirection repo for 3.x
    
    * Add Redirection repo for 2.x
    
    * Fix feedbacks

---
+++ faros-ng/seo-bundle/2.11/src/Entity/Seo/Redirection.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Entity\Seo;
+
+use Doctrine\ORM\Mapping as ORM;
+use Faros\Bundle\SeoBundle\Repository\RedirectionRepository;
+use Faros\Component\Seo\Model\Redirection as RedirectionModel;
+
+/**
+ * @ORM\Entity(repositoryClass=RedirectionRepository::class)
+ * @ORM\Table(name="seo_redirection")
+ */
+#[ORM\Entity(repositoryClass: RedirectionRepository::class)]
+#[ORM\Table(name: 'seo_redirection')]
+class Redirection extends RedirectionModel
+{
+    /**
+     * @var int|null
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    protected $id;
+}
---
+++ faros-ng/seo-bundle/2.11/src/Entity/Seo/Rule.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Entity\Seo;
+
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Faros\Component\Seo\Model\Rule as RuleModel;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="seo_rule")
+ */
+#[ORM\Entity]
+#[ORM\Table(name: 'seo_rule')]
+class Rule extends RuleModel
+{
+    /**
+     * @var int|null
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    protected $id;
+
+    /**
+     * @var Collection<int, RuleException>
+     * @ORM\OneToMany(targetEntity=RuleException::class, mappedBy="rule")
+     */
+    #[ORM\OneToMany(targetEntity: RuleException::class, mappedBy: 'rule')]
+    protected $exceptions;
+}
