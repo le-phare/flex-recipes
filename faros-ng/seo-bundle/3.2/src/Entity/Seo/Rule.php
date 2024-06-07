c06c7081233665b4ab6e673fef1d3e60ab945979
Thibaut Selingue <thibaut.selingue@lephare.com>
2024-06-07 16:47:40 +0200

    feat: add RuleRepository

--- faros-ng/seo-bundle/3.2/src/Entity/Seo/Rule.php
+++ faros-ng/seo-bundle/3.2/src/Entity/Seo/Rule.php
@@ -4,9 +4,10 @@ namespace App\Entity\Seo;
 
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Faros\Bundle\SeoBundle\Repository\RuleRepository;
 use Faros\Component\Seo\Model\Rule as RuleModel;
 
-#[ORM\Entity]
+#[ORM\Entity(repositoryClass: RuleRepository::class)]
 #[ORM\Table(name: 'seo_rule')]
 class Rule extends RuleModel
 {
