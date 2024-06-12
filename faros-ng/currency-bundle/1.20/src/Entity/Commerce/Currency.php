5bd72137b1b00e250f40679c1d51dec905dc40e9
Thibaut Selingue <thibaut.selingue@lephare.com>
2024-06-12 11:35:39 +0200

    fix(recipes): set CurrencyRepository in Currency entity mapping

--- faros-ng/currency-bundle/1.20/src/Entity/Commerce/Currency.php
+++ faros-ng/currency-bundle/1.20/src/Entity/Commerce/Currency.php
@@ -3,16 +3,17 @@
 namespace App\Entity\Commerce;
 
 use Doctrine\ORM\Mapping as ORM;
+use Faros\Bundle\CurrencyBundle\Repository\CurrencyRepository;
 use Faros\Component\Currency\Model\Currency as FarosCurrency;
 use Gedmo\Blameable\Traits\BlameableEntity;
 use Gedmo\Timestampable\Traits\TimestampableEntity;
 use Symfony\Component\Serializer\Annotation as Serializer;
 
 /**
- * @ORM\Entity
+ * @ORM\Entity(repositoryClass=CurrencyRepository::class)
  * @ORM\Table(name="commerce_currency")
  */
-#[ORM\Entity]
+#[ORM\Entity(repositoryClass: CurrencyRepository::class)]
 #[ORM\Table(name: 'commerce_currency')]
 class Currency extends FarosCurrency
 {
--- faros-ng/currency-bundle/3.0/src/Entity/Commerce/Currency.php
+++ faros-ng/currency-bundle/3.0/src/Entity/Commerce/Currency.php
@@ -3,12 +3,13 @@
 namespace App\Entity\Commerce;
 
 use Doctrine\ORM\Mapping as ORM;
+use Faros\Bundle\CurrencyBundle\Repository\CurrencyRepository;
 use Faros\Component\Currency\Model\Currency as FarosCurrency;
 use Gedmo\Blameable\Traits\BlameableEntity;
 use Gedmo\Timestampable\Traits\TimestampableEntity;
 use Symfony\Component\Serializer\Annotation as Serializer;
 
-#[ORM\Entity]
+#[ORM\Entity(repositoryClass: CurrencyRepository::class)]
 #[ORM\Table(name: 'commerce_currency')]
 class Currency extends FarosCurrency
 {
