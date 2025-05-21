f80e2d681f7cf1c3c49ca9571138839cba576d0d
Lucas Mirloup <97196064+lucasmirloup@users.noreply.github.com>
2025-05-21 10:59:41 +0200

    style(php): lint

--- faros-ng/export-bundle/3.8/src/Entity/Export.php
+++ faros-ng/export-bundle/3.8/src/Entity/Export.php
@@ -4,8 +4,8 @@ namespace App\Entity;
 
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use Faros\Component\Export\Model\Export as ExportModel;
 use Faros\Bundle\ExportBundle\ORM\ExportRepository;
+use Faros\Component\Export\Model\Export as ExportModel;
 use Gedmo\Blameable\Traits\BlameableEntity;
 use Gedmo\Timestampable\Traits\TimestampableEntity;
 
