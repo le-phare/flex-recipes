34cd3b84ddfa2f1692c9a760967300bc267021b6
Lucas Mirloup <97196064+lucasmirloup@users.noreply.github.com>
2025-06-12 12:53:24 +0200

    feat(php-cs-fixer): remove unused / redundant rules

--- friendsofphp/php-cs-fixer/3.57/.php-cs-fixer.dist.php
+++ friendsofphp/php-cs-fixer/3.57/.php-cs-fixer.dist.php
@@ -10,20 +10,9 @@ return (new PhpCsFixer\Config())
     ->setParallelConfig(PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect())
     ->setRules([
         '@Symfony' => true,
-        '@DoctrineAnnotation' => true,
-        'no_unused_imports' => true,
-        'array_syntax' => [
-            'syntax' => 'short',
-        ],
-        'ordered_imports' => [
-            'sort_algorithm' => 'alpha',
-        ],
         'multiline_whitespace_before_semicolons' => [
             'strategy' => 'new_line_for_chained_calls',
         ],
-        'phpdoc_separation' => [
-            'groups' => [['ORM\\*'], ['Assert\\*']],
-        ]
     ])
     ->setFinder($finder)
     ->setCacheFile('var/cache/.php-cs-fixer.cache')
