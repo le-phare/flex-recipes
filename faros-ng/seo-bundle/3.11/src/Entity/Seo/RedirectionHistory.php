c19d5e3af80c26072b946544e62f16e46cb44ee1
SimonRethore <srethore.pro@gmail.com>
2025-07-22 13:41:25 +0200

    feat: add RedirectionHistory recipe (#50)
    * 1169425252 - RedirectionHistory recipe
    
    * chore: move to 3.11
    
    * refactor: rename tables
    
    ---------
    
    Co-authored-by: Thibaut Selingue <7049020+thislg@users.noreply.github.com>

---
+++ faros-ng/seo-bundle/3.11/src/Entity/Seo/RedirectionHistory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Entity\Seo;
+
+use Doctrine\ORM\Mapping as ORM;
+use Faros\Bundle\SeoBundle\Repository\RedirectionHistoryRepository;
+use Faros\Component\Seo\Model\RedirectionHistory as RedirectionHistoryModel;
+
+#[ORM\Entity(repositoryClass: RedirectionHistoryRepository::class)]
+#[ORM\Table(name: 'faros_seo_redirection_history')]
+class RedirectionHistory extends RedirectionHistoryModel
+{
+    /**
+     * @var int|null
+     */
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    protected $id;
+}
---
+++ faros-ng/seo-bundle/3.11/src/Entity/Seo/Rule.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Entity\Seo;
+
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Faros\Bundle\SeoBundle\Repository\RuleRepository;
+use Faros\Component\Seo\Model\Rule as RuleModel;
+
+#[ORM\Entity(repositoryClass: RuleRepository::class)]
+#[ORM\Table(name: 'faros_seo_rule')]
+class Rule extends RuleModel
+{
+    /**
+     * @var int|null
+     */
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    protected $id;
+
+    /**
+     * @var Collection<int, RuleException>
+     */
+    #[ORM\OneToMany(targetEntity: RuleException::class, mappedBy: 'rule')]
+    protected $exceptions;
+}
