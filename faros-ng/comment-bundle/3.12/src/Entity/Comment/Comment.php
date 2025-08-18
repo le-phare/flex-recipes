a34fa59fe6678e3f62518e007194206d43be07ad
Thibaut Selingue <7049020+thislg@users.noreply.github.com>
2025-08-18 09:45:12 +0200

    feat: add routes and fix entities

--- faros-ng/comment-bundle/3.12/src/Entity/Comment/Comment.php
+++ faros-ng/comment-bundle/3.12/src/Entity/Comment/Comment.php
@@ -3,11 +3,15 @@
 namespace App\Entity\Comment;
 
 use App\Entity\User;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Faros\Bundle\CommentBundle\Repository\CommentRepository;
+use Faros\Component\Comment\Model\Comment as CommentModel;
+use Faros\Component\Comment\Model\CommentInterface;
 use Gedmo\Blameable\Traits\BlameableEntity;
 use Gedmo\Mapping\Annotation as Gedmo;
 use Gedmo\Timestampable\Traits\TimestampableEntity;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: CommentRepository::class)]
 #[ORM\Table(name: 'faros_comment_comment')]
@@ -16,33 +20,43 @@ use Gedmo\Timestampable\Traits\TimestampableEntity;
 #[ORM\Index(columns: ['created_at'])]
 #[Gedmo\Tree(type: 'closure')]
 #[Gedmo\TreeClosure(class: CommentClosure::class)]
-class Comment extends \Faros\Bundle\CommentBundle\Entity\Comment
+class Comment extends CommentModel
 {
     use BlameableEntity;
     use TimestampableEntity;
 
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    protected $id;
+
+    /**
+     * @var Comment|null
+     */
     #[ORM\ManyToOne(targetEntity: self::class)]
     #[ORM\JoinColumn(nullable: true, onDelete: 'CASCADE')]
     #[Gedmo\TreeParent]
-    protected $parent;
+    protected ?CommentInterface $parent;
 
+    /**
+     * @var Collection<array-key, CommentClosure>
+     */
     #[ORM\OneToMany(targetEntity: CommentClosure::class, mappedBy: 'descendant')]
-    protected $ancestors;
+    protected Collection $ancestors;
 
+    /**
+     * @var Collection<array-key, CommentClosure>
+     */
     #[ORM\OneToMany(targetEntity: CommentClosure::class, mappedBy: 'ancestor')]
-    protected $descendants;
+    protected Collection $descendants;
 
-    /** @var int */
     #[Gedmo\TreeLevel]
-    protected $depth = 0;
+    protected int $depth = 0;
 
-    /** @var User|null */
+    /**
+     * @var User|null
+     */
     #[ORM\ManyToOne(targetEntity: User::class)]
     #[ORM\JoinColumn(nullable: true, onDelete: 'SET NULL')]
-    protected $user;
-
-    public function __construct()
-    {
-        parent::__construct();
-    }
+    protected ?UserInterface $user;
 }
--- faros-ng/comment-bundle/3.12/src/Entity/Comment/CommentClosure.php
+++ faros-ng/comment-bundle/3.12/src/Entity/Comment/CommentClosure.php
@@ -3,22 +3,25 @@
 namespace App\Entity\Comment;
 
 use Doctrine\ORM\Mapping as ORM;
+use Faros\Component\Comment\Model\CommentClosure as CommentClosureModel;
 
 #[ORM\Entity]
 #[ORM\Table(name: 'faros_comment_comment_closure')]
-class CommentClosure extends \Faros\Bundle\CommentBundle\Entity\CommentClosure
+#[ORM\UniqueConstraint(name: 'closure_unique_idx', columns: ['ancestor', 'descendant'])]
+#[ORM\Index(name: 'closure_depth_idx', columns: ['depth'])]
+class CommentClosure extends CommentClosureModel
 {
     /**
      * @var Comment
      */
     #[ORM\ManyToOne(targetEntity: Comment::class, inversedBy: 'descendants', fetch: 'LAZY')]
-    #[ORM\JoinColumn(referencedColumnName: 'id', nullable: false, onDelete: 'CASCADE')]
+    #[ORM\JoinColumn(name: 'ancestor', referencedColumnName: 'id', nullable: false, onDelete: 'CASCADE')]
     protected $ancestor;
 
     /**
      * @var Comment
      */
     #[ORM\ManyToOne(targetEntity: Comment::class, inversedBy: 'ancestors', fetch: 'LAZY')]
-    #[ORM\JoinColumn(referencedColumnName: 'id', nullable: false, onDelete: 'CASCADE')]
+    #[ORM\JoinColumn(name: 'descendant', referencedColumnName: 'id', nullable: false, onDelete: 'CASCADE')]
     protected $descendant;
 }
