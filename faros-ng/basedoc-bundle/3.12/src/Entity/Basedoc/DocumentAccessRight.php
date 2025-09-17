cb4680c6e6476018a3eb514cc3437a7098be7668
Thibaut Selingue <7049020+thislg@users.noreply.github.com>
2025-09-17 11:34:10 +0200

    feat(basedoc-bundle): add 3.12 recipe

---
+++ faros-ng/basedoc-bundle/3.12/src/Entity/Basedoc/DocumentAccessRight.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity\Basedoc;
+
+use Doctrine\ORM\Mapping as ORM;
+use Faros\Bundle\BasedocBundle\Entity\DocumentAccessRight as FarosDocumentAccessRight;
+use Faros\Bundle\BasedocBundle\Entity\DocumentInterface;
+use Gedmo\Blameable\Traits\BlameableEntity;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
+use Symfony\Component\Serializer\Attribute\Groups;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'faros_basedoc_document_access_right')]
+class DocumentAccessRight extends FarosDocumentAccessRight
+{
+    use BlameableEntity;
+    use TimestampableEntity;
+
+    /** @var Document */
+    #[ORM\ManyToOne(targetEntity: Document::class, cascade: ['persist'], inversedBy: 'accessRights')]
+    #[ORM\JoinColumn(nullable: false)]
+    protected DocumentInterface $document;
+
+    #[Groups(['basedoc.folder.get', 'basedoc.document.get'])]
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    #[Groups(['basedoc.folder.get', 'basedoc.document.get'])]
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+}
