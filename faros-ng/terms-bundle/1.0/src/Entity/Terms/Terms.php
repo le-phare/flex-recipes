de37922f9c249387162ec8afd42d0ff271e89090
Aurélien François <aurelien.francois@lephare.com>
2025-09-22 14:04:59 +0200

    add: terms bundle

---
+++ faros-ng/terms-bundle/1.0/src/Entity/Terms/Terms.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Entity\Terms;
+
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Faros\Bundle\TermsBundle\Repository\TermsRepository;
+use Faros\Component\Terms\Model\Terms as TermsModel;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
+
+#[ORM\Entity(repositoryClass: TermsRepository::class)]
+#[ORM\Table(name: 'faros_terms')]
+class Terms extends TermsModel
+{
+    use TimestampableEntity;
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: Types::INTEGER)]
+    protected $id;
+
+    /**
+     * @var Collection<array-key, TermsAcceptation>
+     */
+    #[ORM\OneToMany(targetEntity: TermsAcceptation::class, mappedBy: 'terms', orphanRemoval: true)]
+    protected Collection $termsAcceptations;
+}
