d040038cc15277034ea2837b9f7f9465897fb23a
Thibaut Selingue <7049020+thislg@users.noreply.github.com>
2025-11-04 09:19:16 +0100

    chore: move to 3.14

--- faros-ng/task-bundle/3.14/src/Entity/Task/Task.php
+++ faros-ng/task-bundle/3.14/src/Entity/Task/Task.php
@@ -4,17 +4,17 @@ namespace App\Entity\Task;
 
 use Doctrine\ORM\Mapping as ORM;
 use Faros\Bundle\TaskBundle\Repository\TaskRepository;
-use Faros\Component\EventLog\Traits\TimestampableImmutableEntity;
 use Faros\Component\Task\Model\Task as TaskModel;
 use Faros\Component\Task\Model\TaskInterface;
 use Gedmo\Blameable\Traits\BlameableEntity;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
 
 #[ORM\Entity(repositoryClass: TaskRepository::class)]
-#[ORM\Table(name: 'task')]
+#[ORM\Table(name: 'faros_task_task')]
 class Task extends TaskModel implements TaskInterface
 {
     use BlameableEntity;
-    use TimestampableImmutableEntity;
+    use TimestampableEntity;
 
     /**
      * @var int|null
