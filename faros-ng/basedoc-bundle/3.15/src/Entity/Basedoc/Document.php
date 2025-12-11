aa77375987dbdaadf2a6dc433dc59afa85185394
Thibaut Selingue <7049020+thislg@users.noreply.github.com>
2025-12-11 09:47:20 +0100

    feat: add basedoc-bundle 3.15

---
+++ faros-ng/basedoc-bundle/3.15/src/Entity/Basedoc/Document.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Entity\Basedoc;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Faros\Bundle\BasedocBundle\Entity\Document as FarosDocument;
+use Faros\Bundle\BasedocBundle\Repository\DocumentRepository;
+use Gedmo\Blameable\Traits\BlameableEntity;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
+use Symfony\Component\Serializer\Attribute\Groups;
+use Symfony\Component\Serializer\Attribute\MaxDepth;
+
+#[ORM\Entity(repositoryClass: DocumentRepository::class)]
+#[ORM\Table(name: 'faros_basedoc_document')]
+class Document extends FarosDocument
+{
+    use BlameableEntity;
+    use TimestampableEntity;
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    protected ?int $id = null;
+
+    /**
+     * @var Collection<array-key, Folder>
+     */
+    #[ORM\JoinTable(name: 'faros_basedoc_folder_document')]
+    #[ORM\InverseJoinColumn(name: 'folder_id', referencedColumnName: 'id')]
+    #[ORM\ManyToMany(targetEntity: Folder::class, inversedBy: 'documents')]
+    #[Groups(['basedoc.folder.get', 'basedoc.document.get'])]
+    #[MaxDepth(1)]
+    protected Collection $folders;
+
+    /**
+     * @var Collection<array-key, DocumentAccessRight>
+     */
+    #[ORM\OneToMany(targetEntity: DocumentAccessRight::class, mappedBy: 'document', cascade: ['persist', 'remove'], orphanRemoval: true)]
+    #[Groups(['basedoc.folder.get', 'basedoc.document.get'])]
+    protected Collection $accessRights;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->folders = new ArrayCollection();
+        $this->accessRights = new ArrayCollection();
+    }
+
+    #[Groups(['basedoc.folder.get', 'basedoc.document.get'])]
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    #[Groups(['basedoc.folder.get', 'basedoc.document.get'])]
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+}
