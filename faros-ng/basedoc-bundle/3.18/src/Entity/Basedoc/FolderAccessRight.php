a8fdc7fe087d9cb199fa51079b0b6238b2520b74
Lucas Mirloup <97196064+lucasmirloup@users.noreply.github.com>
2026-03-26 14:45:05 +0100

    fix(basedoc-bundle): Folder PHP generics (#103)
    * fix(basedoc-bundle): Folder PHP generics
    
    * fix: add missing MaxDepth
    
    ---------
    
    Co-authored-by: Thibaut Selingue <7049020+thislg@users.noreply.github.com>

---
+++ faros-ng/basedoc-bundle/3.18/src/Entity/Basedoc/FolderAccessRight.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Entity\Basedoc;
+
+use Doctrine\ORM\Mapping as ORM;
+use Faros\Bundle\BasedocBundle\Entity\FolderAccessRight as FarosFolderAccessRight;
+use Faros\Bundle\BasedocBundle\Entity\FolderInterface;
+use Gedmo\Blameable\Traits\BlameableEntity;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
+use Symfony\Component\Serializer\Attribute\Groups;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'faros_basedoc_folder_access_right')]
+class FolderAccessRight extends FarosFolderAccessRight
+{
+    use BlameableEntity;
+    use TimestampableEntity;
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    protected ?int $id = null;
+
+    /** @var Folder */
+    #[ORM\ManyToOne(targetEntity: Folder::class, cascade: ['persist'], inversedBy: 'accessRights')]
+    #[ORM\JoinColumn(nullable: false)]
+    protected FolderInterface $folder;
+
+    #[Groups(['basedoc.folder.get', 'basedoc.document.get'])]
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    #[Groups(['basedoc.folder.get', 'basedoc.document.get'])]
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+}
